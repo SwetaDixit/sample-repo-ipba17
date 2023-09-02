3808845ed0a0edaee71b5b15ebf9f8d8e3790bd1
SwetaDixit <143870019+SwetaDixit@users.noreply.github.com>
2023-09-02 13:18:34 +0530

    Add files via upload

---
+++ Data_Preprocessing_Day2.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(ggplot2)
+library(DataExplorer)
+library(summarytools)
+library(reshape2)  
+
+#Data Import
+job_data <- read.csv("https://raw.githubusercontent.com/KunaalNaik/R_Basics/main/IPBA%2017/job_change.csv")
+
+
+##### Check Summary Statistics
+
+
+##### Check Missing Values
+
+
+
+##### Numerical
+
+# Treat - training_hours
+
+# Check
+
+
+
+# Treat - city_development_index
+
+# Check
+
+
+
+
+##### Categorical
+
+# Treat - major_discipline
+
+# Bar Plot to Check
+
+
+# Treat - education_level
+
+
